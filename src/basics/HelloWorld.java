13a297dbc37abfb22e376da57f6206c32223cff9
jfc4050 <31414860+jfc4050@users.noreply.github.com>
2017-08-29 14:38:42 -0700

    Update HelloWorld.java

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("adding code to project");
+		System.out.println("adding code from github");
 	}
 }
